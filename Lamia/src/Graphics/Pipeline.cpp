8390c2e1ea16175f279f0d61b2588f03fee45025
Alora Boone <39552213+Alorafae@users.noreply.github.com>
2019-07-25 16:29:54 -0700

    Pipeline class methods.

--- Lamia/src/Graphics/Pipeline.cpp
+++ Lamia/src/Graphics/Pipeline.cpp
@@ -8,3 +8,28 @@ LamiaPipeline::LamiaPipeline()
 LamiaPipeline::~LamiaPipeline()
 {
 }
+
+VkResult LamiaPipeline::CreateDescriptorPipelineLayout(DeviceInfo & di, bool textured, VkDescriptorSetLayoutCreateFlags descSetLayoutCreateFlags)
+{
+  return VkResult();
+}
+
+VkResult LamiaPipeline::CreateDescriptorPool(DeviceInfo & di, bool textured)
+{
+  return VkResult();
+}
+
+VkResult LamiaPipeline::CreateDescriptorSet(DeviceInfo & di, bool textured)
+{
+  return VkResult();
+}
+
+VkResult LamiaPipeline::CreatePipelineCache(DeviceInfo & di)
+{
+  return VkResult();
+}
+
+VkResult LamiaPipeline::CreatePipeline(DeviceInfo & di, VkBool32 depth, VkBool32 vertexInput, bool textured)
+{
+  return VkResult();
+}
--- Lamia/src/Graphics/Pipeline.h
+++ Lamia/src/Graphics/Pipeline.h
@@ -9,6 +9,14 @@ class LamiaPipeline
     LamiaPipeline();
     ~LamiaPipeline();
 
+    VkResult CreateDescriptorPipelineLayout(DeviceInfo &di, bool textured, VkDescriptorSetLayoutCreateFlags descSetLayoutCreateFlags = 0);
+    VkResult CreateDescriptorPool(DeviceInfo &di, bool textured);
+    VkResult CreateDescriptorSet(DeviceInfo &di, bool textured);
+
+    VkResult CreatePipelineCache(DeviceInfo &di);
+
+    VkResult CreatePipeline(DeviceInfo &di, VkBool32 depth, VkBool32 vertexInput, bool textured);
+
   private:
     VkPipelineLayout pipeLayout;
     VkPipelineCache pipeCache;
