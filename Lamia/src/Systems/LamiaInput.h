15c3e9268421c47470b1ea373ab3dd2586154eba
Alora Boone <39552213+Alorafae@users.noreply.github.com>
2020-10-12 12:29:10 -0700

    Buffered raw input read is correctly reading multiple key presses at once. Added a keyboard class as well but not sure if I'll use it yet or not.

--- Lamia/src/Systems/LamiaInput.h
+++ Lamia/src/Systems/LamiaInput.h
@@ -2,6 +2,16 @@
 #pragma once
 #include <Windows.h> // need this included in less files which is part of sorting out the whole window situation
 
+// class to create and keep track of the real-time state of the keyboard
+// maybe just use an array that can hold each VK
+class LamiaKeyboard
+{
+  public:
+
+  private:
+
+};
+
 class LamiaInput
 {
   public:
